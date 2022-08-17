d4bb20f49732745ea77ba78a397045eb3b1afa46
yyyng2 <yyyng2@gmail.com>
2022-08-17 12:41:16 +0900

    Initial Commit

---
+++ SeSACCodeBaseUIPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACCodeBaseUIPractice
+//
+//  Created by Y on 2022/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
